d29ecb9f9fde3a5c92f98c00bf32f0746f3a6603
ncardenasM31 <ncardenas2016@gmail.com>
2026-09-21 14:54:09 -0600

    se suspendieron algunos botones en home

--- js/vistas/home.js
+++ js/vistas/home.js
@@ -90,9 +90,9 @@ function renderHomeUsuario(container, user) {
       <h2 style="font-size: 40px">Aquí tienes algunos de tus juegos favoritos</h2>
       <div class="game-buttons">
         <button data-game="calculoMental">🧮 Cálculo Mental</button>
-        <button data-game="balloons">🧮 Balloons </button>
+        <!--<button data-game="balloons">🧮 Balloons </button>-->
         <button data-game="loteria">🧮 Loteria </button>
-        <button data-game="wordless">🧮 Mathless </button>
+        <!--<button data-game="wordless">🧮 Mathless </button>-->
         <!--<button data-game="pokemat">🧮 Pokemat </button>-->
       </div>
     </section>
